0f7de4fb13268d4ba13c148ddd38e18db6824625
kodex-07 <adegbiteadedayo48@gmail.com>
2026-09-23 12:13:01 +0100

    updated website"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/react-movie-app",
+  };
+});
