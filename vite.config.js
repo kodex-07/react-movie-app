57e146946e8caf699ef5c1ceb9d6cc5edc4cb0f5
kodex-07 <adegbiteadedayo48@gmail.com>
2026-09-25 10:43:15 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    base: env.VITE_BASE_PATH || "/movie-app",
+    base: env.VITE_BASE_PATH || "/react-movie-app",
   };
 });
